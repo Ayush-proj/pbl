a931bb8c13a89187ffb0ec14653c1a3c14271fd8
Ayush Bhosale <ayushrbhosale2004@gmail.com>
2026-05-18 13:48:40 +0530

    Restrict CORS origin to specific URL

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Global Middlewares
 // --------------------
 app.use(cors({
-  origin: true,
+  origin:"https://mentorconnect-hazel.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
